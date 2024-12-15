818b81006762bf7bf3d624c478781483a5a05f9d
Anthony Stirling <77850077+Frooodle@users.noreply.github.com>
2024-12-15 23:49:24 +0000

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,7 +25,7 @@ const config = {
         defaultLocale: 'en',
         locales: ['en'],
     },
-
+    themes: ['@docusaurus/theme-search-algolia'],
     presets: [
         [
             'classic',
